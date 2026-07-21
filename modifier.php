6494bc2f0a99b132780dda4e6209041de01bb817
Noariantsoa <zonoariantsoa@gmail.com>
2026-07-21 08:33:39 +0300

    modif vita

--- modifier.php
+++ modifier.php
@@ -13,14 +13,14 @@ $choice = $_GET['choix'];
 <body>
     <h3>Veuilez remplir le formulaire suivant :</h3>
     <?php if ( $choice == "modify") {?>
-        <form action="traitement_modif.php" method="get">
-            <input type="hidden" name="id_produit" value="<?php echo $_GET['id'] ?>">
-            <p>Entrez le nouveau nom : <input type="text" name="new_name"></p>
-            <p>Choisir l'id de la nouvelle categorie : <input type="text" name="new_categorie"></p>
-            <p>Entrez le nouveau prix de reference : <input type="number" name="new_prix"></p>
-            <p>Perime <input type="checkbox" name="peremption" ></input></p>
-            <input type="submit" value="Entrez"></input>
-        </form>
+            <form action="traitement_modif.php" method="get">
+                <input type="hidden" name="id_produit" value="<?php echo $_GET['id'] ?>">
+                <p>Entrez le nouveau nom : <input type="text" name="new_name"></p>
+                <p>Choisir l'id de la nouvelle categorie : <input type="text" name="new_categorie"></p>
+                <p>Entrez le nouveau prix de reference : <input type="number" name="new_prix"></p>
+                <p>Perime <input type="checkbox" name="peremption" ></input></p>
+                <input type="submit" value="Entrez"></input>
+            </form>
     <?php } ?>
     <?php if ( $choice == "add") {?>
         <form action="traitement_modif.php" method="get">
